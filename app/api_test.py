6746784cc1effc89c02b1958695c68ddef481c19
margosq <slava1kvartovkin@gmail.com>
2023-01-28 11:59:30 +0300

    test(api): add zero check in mean

--- app/api_test.py
+++ app/api_test.py
@@ -23,26 +23,32 @@ def test_mean():
     assert response.json() == {"mean": 28.09}
 
 
-def test_empty_count_with_timestamp():
+def test_empty_count_with_from_timestamp():
     response = client.get("/count?from_timestamp=1674811012")
     assert response.status_code == 200
     assert response.json() == {"count": 0}
 
 
-def test_count_with_timestamp():
+def test_count_with_from_timestamp():
     response = client.get("/count?from_timestamp=1674306330")
     assert response.status_code == 200
     assert response.json() == {"count": 8}
 
 
-def test_max_with_timestamp():
+def test_max_with_to_timestamp():
     response = client.get("/max?to_timestamp=1574749567")
     assert response.status_code == 200
     assert response.json() == {"max": 82}
 
 
-def test_mean_with_timestamp():
+def test_mean_with_from_to_timestamp():
     response = client.get(
         "/mean?from_timestamp=1274749567&to_timestamp=1524739567")
     assert response.status_code == 200
     assert response.json() == {"mean": 22.7}
+
+
+def test_zero_mean_with_from_timestamp():
+    response = client.get("/mean?from_timestamp=1674895969")
+    assert response.status_code == 200
+    assert response.json() == {"mean": 0}
